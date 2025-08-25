bb0d863bf74d6e245531c3c90affc20eb672b6cc
newbie0z-lol <anhduyn937@gmail.com>
2025-08-25 22:27:47 +0700

    preprt.lua

--- preprt.lua
+++ preprt.lua
@@ -1,2 +1,12 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/anhlamgixungdangvoiem/checkkeypremium/refs/heads/main/checkkeypre.lua"))()
--- Đòi Đá Đểu Script Bố Mày?
+
+if game.PlaceId == 286090429 then
+    -- Arsenal
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/anhlamgixungdangvoiem/asenal/refs/heads/main/rezcdvn.lua"))()
+
+elseif game.PlaceId == 18192562963 then
+    -- Cộng Đồng Việt Nam
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/newbie0z-lol/preprt/refs/heads/main/preprt.lua"))()
+
+end
+
+-- Ô lạc à chú em , đi về đi dumper hay spy cũng không có gì cho mày chơi đâu xéo dùm cái :]] yêu :33
