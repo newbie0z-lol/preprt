67e7632f27b224c35595e6ee84820d0eca595dad
newbie0z-lol <anhduyn937@gmail.com>
2025-09-14 17:15:11 +0700

    preprt.lua

--- preprt.lua
+++ preprt.lua
@@ -7,9 +7,9 @@ elseif game.PlaceId == 18192562963 then
     -- Cộng Đồng Việt Nam
     loadstring(game:HttpGet("https://raw.githubusercontent.com/anhlamgixungdangvoiem/H-Ch-Minh/refs/heads/main/New%20Text%20Document%20(2).txt"))()
 
-elseif game.PlaceId == 79546208627805 then
-	-- 99 Night
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/anhlamgixungdangvoiem/checkkey99/refs/heads/main/99night%20rezhub.lua"))()
+elseif game.PlaceId == 126509999114328 then
+    -- Tên Game (tùy bạn đặt)
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/anhlamgixungdangvoiem/checkkey99/refs/heads/main/99night%20rezhub.lua"))()
 end
 
 -- Ô lạc à chú em , đi về đi dumper hay spy cũng không có gì cho mày chơi đâu xéo dùm cái :]] yêu :33
