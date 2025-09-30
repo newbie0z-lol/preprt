2edc310c8f27f9eddf6d2ed5e68f992e330d6a40
newbie0z-lol <anhduyn937@gmail.com>
2025-09-30 11:29:15 +0700

    preprt.lua

--- preprt.lua
+++ preprt.lua
@@ -14,6 +14,10 @@ elseif game.PlaceId == 126509999114328 then
 elseif game.PlaceId == 94941998730756 then
     -- Cộng Đồng Việt Nam Var Đơn
     loadstring(game:HttpGet("https://raw.githubusercontent.com/anhlamgixungdangvoiem/H-Ch-Minh/refs/heads/main/New%20Text%20Document%20(2).txt"))()
+	
+elseif game.PlaceId == 142823291 then
+    -- MM2
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/anhlamgixungdangvoiem/MM2z/refs/heads/main/mm2.lua"))()
 end
 
 -- Ô lạc à chú em , đi về đi dumper hay spy cũng không có gì cho mày chơi đâu xéo dùm cái :]] yêu :33
