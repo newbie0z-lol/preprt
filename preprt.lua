0b789e8558d8aa3a20723571ffaa7cedc79f8217
newbie0z-lol <anhduyn937@gmail.com>
2025-11-29 20:43:50 +0700

    Update preprt.lua

--- preprt.lua
+++ preprt.lua
@@ -6,7 +6,7 @@ task.wait(10)
 local gameList = {
     [286090429]       = "https://raw.githubusercontent.com/anhlamgixungdangvoiem/asenal/refs/heads/main/rezcdvn.lua", -- Arsenal
     [18192562963]     = "https://raw.githubusercontent.com/rezhubz/CDVN/refs/heads/main/rez.lua", -- CDVN
-    [126509999114328] = "https://raw.githubusercontent.com/anhlamgixungdangvoiem/99night/refs/heads/main/99%20night%20(2).lua", -- 99 Night
+    [126509999114328] = "https://raw.githubusercontent.com/rezhubz/99-Night/refs/heads/main/rez.lua", -- 99 Night
 	[79546208627805] = "https://raw.githubusercontent.com/anhlamgixungdangvoiem/intro-99-night/refs/heads/main/loader%2099%20night.lua", -- 99 Night Sảnh
     [94941998730756]  = "https://raw.githubusercontent.com/anhlamgixungdangvoiem/H-Ch-Minh/refs/heads/main/New%20Text%20Document%20(2).txt", -- CDVN Var Đơn
     [80469437126309]  = "https://raw.githubusercontent.com/anhlamgixungdangvoiem/MM2z/refs/heads/main/mm2.lua", -- MM2
